3fd5a0a13de2dcef2bd12a81d83136d19fc6e2ec
xytuang <xytuang@node0.xytuang-296525.e6897-pg0.utah.cloudlab.us>
2026-03-24 14:40:37 -0600

    Add minio setup script

---
+++ setup_minio.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install and start docker
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+mkdir -p /mydata/minio-data
+
+sudo docker run -d \
+  --name minio \
+  -p 9000:9000 \
+  -p 9001:9001 \
+  -v /mydata/minio-data:/data \
+  -e MINIO_ROOT_USER=minio \
+  -e MINIO_ROOT_PASSWORD=minio123 \
+  minio/minio server /data --console-address ":9001"
+
+# Install minio client and create Milvus bucket
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+sudo mv mc /usr/local/bin/
+
+mc alias set local http://localhost:9000 minio minio123
+
+mc mb local/milvus
+
+
+
+
+
+
+
+
+
