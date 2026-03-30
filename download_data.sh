9b1dcf59bc318f2a8e36f836349d82939ab1e5fb
xytuang <xiangyutuang@gmail.com>
2026-03-30 04:59:53 -0600

    Update scripts for 500M

--- download_data.sh
+++ download_data.sh
@@ -9,16 +9,17 @@ git lfs install
 GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/microsoft/SPTAG.git /mydata/SPTAG
 
 cd /mydata/SPTAG
-git lfs pull --include "datasets/SPACEV1B/vectors.bin/vectors_1.bin"
-git lfs pull --include "datasets/SPACEV1B/vectors.bin/vectors_2.bin"
-git lfs pull --include "datasets/SPACEV1B/vectors.bin/vectors_3.bin"
+
+for i in {1..15}; do
+    git lfs pull --include "datasets/SPACEV1B/vectors.bin/vectors_$i.bin"
+done
 
 git lfs pull --include "datasets/SPACEV1B/query.bin"
 git lfs pull --include "datasets/SPACEV1B/truth.bin"
 
 cd datasets/SPACEV1B/vectors.bin/
 mv vectors_1.bin vectors_merged.bin
-for i in {2..3}; do
+for i in {2..15}; do
 	cat vectors_$i.bin >> vectors_merged.bin
 	rm vectors_$i.bin
 done
