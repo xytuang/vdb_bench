478e6450379b661263b296d9a512534f58a4db78
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-04-04 08:56:29 -0600

    feat: Add script to set up master and yaml configs

--- distributed_setup/setup_master.sh
+++ distributed_setup/setup_master.sh
@@ -2,3 +2,12 @@
 
 ./common.sh
 ./master_only.sh
+
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl get sc
+
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml
+kubectl get pods -n cert-manager
+
+kubectl apply -f https://raw.githubusercontent.com/zilliztech/milvus-operator/main/deploy/manifests/deployment.yaml
