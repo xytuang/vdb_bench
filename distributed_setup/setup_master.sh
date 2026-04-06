6f91b6302ac84f17e3aae66e2b0b0ef4796af68e
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-04-06 06:56:38 -0600

    fix: Change labels to suit number of nodes

--- distributed_setup/setup_master.sh
+++ distributed_setup/setup_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#./common.sh
-#./master_only.sh
+./common.sh
+./master_only.sh
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
